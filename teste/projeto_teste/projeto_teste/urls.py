c3fc3bdc3d109b5e05b5d2194ced4a29b654f166
LeandroPS121 <dleandropaulo@gmail.com>
2024-07-15 13:35:24 -0300

    a

--- teste/projeto_teste/projeto_teste/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('nome_app.urls')),  # substitua 'nome_app' pelo nome do seu aplicativo
-    path('registrar/', include('nome_app.urls')),  # inclua as rotas do seu aplicativo, se necessário
-    path('tabela/', include('nome_app.urls')),  # inclua as rotas do seu aplicativo, se necessário
-]
